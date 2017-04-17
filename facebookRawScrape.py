4171d28e479609db78a4337c7dda54ad0a243ca6
jonpi1 <pi@raspberrypi.(none)>
2017-04-17 19:29:13 +0000

    Currently trying to write output from beautiful soup to a file, getting error.

--- facebookRawScrape.py
+++ facebookRawScrape.py
@@ -8,6 +8,9 @@ def testFacebookScrape(search):
 	#r = requests.get("https://www.facebook.com/public?query=" + search + "&init=ffs&nomc=0")
 	page = urllib2.urlopen("https://www.facebook.com/public?query=" + search + "&init=ffs&nomc=0")
 	soup = BeautifulSoup(page, 'lxml')
-	print(soup.prettify())
+	
+	f = open('result.txt', 'w')
+	f.write(str(soup.get_text()))
+	f.close()
 
 testFacebookScrape("jonathan")	
