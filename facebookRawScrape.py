4ce1b968f7bff61231deb9c3d4a807ef1fecf866
jtfree <jonfree97@gmail.com>
2017-05-14 23:10:03 +0000

    Version 1.0
    - Currently only scrapes the name of the user and a link to their profile.
    - Prints out five different text files, one for each page of listing.
    - Duplicate entries exist, need to fix this.

--- facebookRawScrape.py
+++ facebookRawScrape.py
@@ -3,14 +3,53 @@
 import urllib2
 import requests
 from bs4 import BeautifulSoup
+from bs4 import Comment
 
-def testFacebookScrape(search):
-	#r = requests.get("https://www.facebook.com/public?query=" + search + "&init=ffs&nomc=0")
-	page = urllib2.urlopen("https://www.facebook.com/public?query=" + search + "&init=ffs&nomc=0")
-	soup = BeautifulSoup(page, 'lxml')
-	
-	f = open('result.txt', 'w')
-	f.write(soup.prettify().encode("UTF-8"))
-	f.close()
+def FacebookScrape(search, pageNumber):
+        page = urllib2.urlopen("https://www.facebook.com/public?query=" + search + "&init=ffs&nomc=0&page=" + pageNumber)
+        soup = BeautifulSoup(page, 'lxml')
+        comments = soup.find_all(string=lambda text:isinstance(text,Comment))
 
-testFacebookScrape("jonathan")	
+        comment = comments[1];
+    
+        soup2 = BeautifulSoup(comment, 'lxml')
+
+        linkTags = soup2.find_all('a')
+        
+        stringLinks=[]
+        
+        for link in linkTags:
+            stringLinks.append(str(link))
+        
+        profileLinks=[]
+        
+        for link in stringLinks:
+            if "data-testid" in link:
+                profileLinks.append(link)
+                
+        trueLinks = [];
+        
+        
+        for link in profileLinks:
+            soup3 = BeautifulSoup(link, 'lxml')
+            trueLinks.append(soup3.find('a').get('href'))    
+        
+        f = open('result' + pageNumber + '.txt', 'w')
+        f.write("Name   Link \n")
+            
+        for link in trueLinks:
+            profileScrape(link, f)
+            
+        f.close()
+
+            
+            
+def profileScrape(link, f):
+    print("Starting scrape for " + link)
+    page = urllib2.urlopen(link)
+    soup = BeautifulSoup(page, 'lxml')
+    name = soup.find('a', class_='_8_2').text
+    f.write((name + " " + link + "\n").encode("UTF-8"))
+#       f = open('result.txt', 'w')
+#       f.write(links.encode("UTF-8"))
+#       f.close()
---
+++ master.py
@@ -0,0 +1,4 @@
+from facebookRawScrape import FacebookScrape, profileScrape
+
+for x in range(1, 6):
+    FacebookScrape("jonathan", str(x))
